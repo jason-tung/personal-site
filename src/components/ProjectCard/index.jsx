15c66a6d5085c679c25bfa0cd9aa4035b9e7adb9
Jason Tung <jt568@cornell.edu>
2020-10-09 18:46:57 -0400

    card component done!

--- src/components/ProjectCard/index.jsx
+++ src/components/ProjectCard/index.jsx
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, {useRef} from 'react'
+import {Typography} from '@material-ui/core'
+import styles from './index.module.css';
 
-export default () => <div>
-  
-</div>
+export default ({title, link, image, image2, children}) => {
+  const img2 = useRef();
+  return <a href={link} target="_blank" rel="noopener noreferrer">
+    <div className = {styles.card} 
+    onMouseEnter={() => {img2.current.style.opacity = 1}}
+    onMouseLeave={() => {img2.current.style.opacity = 0}}
+
+    >
+      <Typography variant = "h5" className = {styles.title}>
+        {title}
+      </Typography>
+      <div className={styles.imgdiv}>
+        <div className = {styles.innerimgdiv}>
+      <img src = {image2} ref = {img2} alt = {`${title} gui`} className = {`${styles.pic2} ${styles.pic}`}/>
+      <img src = {image} alt = {`${title} gui`} className = {styles.pic}/>
+      </div>
+      </div>
+      <Typography variant = "body2" className = {styles.body}>
+        {children}
+      </Typography>
+    </div>
+  </a>
+}
--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Typography} from '@material-ui/core'
 import styles from './index.module.css'
+import samwise1 from '../../assets/samwise-splash.png'
+import samwise2 from '../../assets/samwise-use.png'
+import ProjectCard from '../ProjectCard'
 
 export default () => <div className = {styles.contain}>
       <Typography variant="h4" align="left" >What have I made?</Typography>
       <Typography variant ="body1" aligh = "left" className = {styles.aboutme}>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Et netus et malesuada fames ac turpis. Mi quis hendrerit dolor magna eget est lorem. Volutpat lacus laoreet non curabitur gravida arcu ac tortor.      </Typography>
- 
+      <div className = {styles.pjdiv}>
+      <ProjectCard {...{title: "Samwise", link: "https://github.com/cornell-dti/samwise/", image: samwise1, image2: samwise2}}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi quis hendrerit dolor magna eget est lorem ipsum dolor.
+      </ProjectCard>
+      </div>
 </div>
