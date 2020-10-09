00a4a5e8d28cb668995444c06abff4cb0b8d74c4
Jason Tung <jt568@cornell.edu>
2020-10-09 16:53:40 -0400

    layout

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,19 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Intro from '../Intro';
 import FunnySans from '../FunnySans';
 import Links from '../Links';
 import About from '../About';
+import WhatIDo from '../WhatIDo';
+import Projects from '../Projects';
 import styles from './index.module.css'
 
 export default () => {
 
-  const [light, setLight] = useState(false);
-  return <div className={`App ${styles.main} ${light ? styles.light : styles.dark}`}>
+  return <div className={`App ${styles.main} ${styles.dark}`}>
     <div className ={styles.cont}>
       <FunnySans />
       <Intro />
       <Links />
       <About />
+      <WhatIDo />
+      <Projects />
     </div>
   </div>
 }
---
+++ src/components/WhatIDo/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+import styles from './index.module.css'
+
+export default () => <div className = {styles.contain}>
+      <Typography variant="h4" align="left" >What have I done?</Typography>
+      <Typography variant ="body1" aligh = "left" className = {styles.aboutme}>
+      Woah woah woah.. let's start with
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mi quis hendrerit dolor magna eget est lorem ipsum dolor. Et netus et malesuada fames ac turpis. Mi quis hendrerit dolor magna eget est lorem. Volutpat lacus laoreet non curabitur gravida arcu ac tortor. 
+      </Typography>
+      <Typography variant ="body1" aligh = "left" className = {styles.aboutme}>
+      In the past, I have
+       Lorem ipsum dolor sit amet, consectetur adipiscing elit      
+      </Typography>
+
+</div>
