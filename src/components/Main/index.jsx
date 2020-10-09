a851ee1ec8ea7b8f51396adefa23ce0220c61d8a
Jason Tung <jt568@cornell.edu>
2020-10-09 15:32:17 -0400

    about finished

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import Intro from '../Intro';
 import FunnySans from '../FunnySans';
 import Links from '../Links';
+import About from '../About';
 import styles from './index.module.css'
 
 export default () => {
 
   const [light, setLight] = useState(false);
   return <div className={`App ${styles.main} ${light ? styles.light : styles.dark}`}>
-    <div>
+    <div className ={styles.cont}>
       <FunnySans />
       <Intro />
       <Links />
+      <About />
     </div>
   </div>
 }
