14dfc62c1f77293e5f925744cf7e3e572726aae6
jason-tung <jt568@cornell.edu>
2021-11-13 08:53:27 -0500

    mend

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Main from './components/Main';
 import Secret from './components/Secret';
 
@@ -11,6 +11,11 @@ function App() {
         <Switch>
           <Route path="/secret">
             <Secret />
+          </Route>
+          <Route path="/todo">{() => { 
+            window.location.href = 'https://docs.google.com/document/d/14qJtspTNkCxQXGH6DP_T2X3kjIoc-Bj-lPLP7PuXmYE/edit'; 
+            return null;
+              }}    
           </Route>
           <Route path="/">
             <Main />
